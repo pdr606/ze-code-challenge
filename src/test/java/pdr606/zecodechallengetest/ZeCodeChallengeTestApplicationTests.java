cf4e881406174b31b923fbecbf09088ce9da9fa6
Pedro Guarnieri <pedroguarnieric@gmail.com>
2023-09-25 21:18:07 -0300

    testes

--- src/test/java/pdr606/zecodechallengetest/ZeCodeChallengeTestApplicationTests.java
+++ src/test/java/pdr606/zecodechallengetest/ZeCodeChallengeTestApplicationTests.java
@@ -3,11 +3,11 @@ package pdr606.zecodechallengetest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ZeCodeChallengeTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	
+
+
 
 }
